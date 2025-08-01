53d904d5455aa10a786a9fc755ba6b943e5e711f
hadiahmad06 <162732770+hadiahmad06@users.noreply.github.com>
2025-07-31 21:48:32 -0500

    Initial Commit

---
+++ Cortex/CortexApp.swift
@@ -0,0 +1,17 @@
+//
+//  CortexApp.swift
+//  Cortex
+//
+//  Created by Hadi Ahmad on 7/31/25.
+//
+
+import SwiftUI
+
+@main
+struct CortexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
